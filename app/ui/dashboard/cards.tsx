a808f8a6926617b7c86b36b21787dcc5258daa87
mateico <mateo.rial@gmail.com>
2025-10-07 15:57:14 -0300

    first commit for rt web

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -4,6 +4,7 @@ import {
   UserGroupIcon,
   InboxIcon,
 } from '@heroicons/react/24/outline';
+import { fetchCardData } from '@/app/lib/data';
 import { lusitana } from '@/app/ui/fonts';
 
 const iconMap = {
@@ -14,20 +15,24 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
-  return (
-    <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
-    </>
-  );
+    const {
+        numberOfInvoices,
+        numberOfCustomers,
+        totalPaidInvoices,
+        totalPendingInvoices,
+    } = await fetchCardData();
+    return (
+        <>
+            <Card title="Collected" value={totalPaidInvoices} type="collected"/>
+            <Card title="Pending" value={totalPendingInvoices} type="pending"/>
+            <Card title="Total Invoices" value={numberOfInvoices} type="invoices"/>
+            <Card
+                title="Total Customers"
+                value={numberOfCustomers}
+                type="customers"
+            />
+        </>
+    );
 }
 
 export function Card({
