64c87fcfad4ed63c49f17ba19c3d29e055315e36
mateico <mateo.rial@gmail.com>
2025-10-09 16:58:03 -0300

    new title added

--- app/ui/rf-logo.tsx
+++ app/ui/rf-logo.tsx
@@ -3,7 +3,7 @@ import { lusitana } from '@/app/ui/fonts';
 
 export default function RFLogo({ className }: { className?: string }) {
   return <img
-      src="/rufina_travel_logo_large.svg"
+      src="/rufina_travel_title.svg"
       alt="Rufina Travel"
       className={className ?? 'h-10 w-auto'} />;
 }
