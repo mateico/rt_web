2004917466db84ed1d977e8f4222fdcc739c84c1
mateico <mateo.rial@gmail.com>
2025-10-11 17:58:53 -0300

    color strip done

--- app/ui/TopNavBar.tsx
+++ app/ui/TopNavBar.tsx
@@ -1,17 +1,29 @@
 'use client';
 
 import RFLogo from '@/app/ui/rf-logo';
+import ColorfullBar from '@/app/ui/colorfullBar';
 import NavLinks from '@/app/ui/nav-links'; // this is the one you already have
 
 export default function TopNavBar() {
     return (
-        <header className="flex items-center justify-between bg-secondary-30 px-6 py-4 shadow-md">
-            <div className="flex items-center gap-4">
-                <RFLogo className="h-10 w-auto" />
+        <header className="flex   bg-secondary-30 shadow-md">
+
+            <div className="flex flex-col w-full">
+                <div className="flex items-center justify-between px-6 py-4">
+                    <div className="flex items-center gap-4">
+                        <RFLogo className="h-10 w-auto" />
+                    </div>
+                    <nav className="flex gap-4">
+                        <NavLinks />
+                    </nav>
+                </div>
+                <ColorfullBar />
             </div>
-            <nav className="flex gap-4">
-                <NavLinks />
-            </nav>
+
+
+            {/*<ColorfullBar />*/}
+
+
         </header>
     );
 }
