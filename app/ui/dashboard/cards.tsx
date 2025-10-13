8d8bc6bcdb10212a2ecd47f9347848f4bb1149c0
mateico <mateo.rial@gmail.com>
2025-10-12 22:24:50 -0300

    commenting queries

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -15,22 +15,22 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
-    const {
+    /*const {
         numberOfInvoices,
         numberOfCustomers,
         totalPaidInvoices,
         totalPendingInvoices,
-    } = await fetchCardData();
+    } = await fetchCardData();*/
     return (
         <>
-            <Card title="Collected" value={totalPaidInvoices} type="collected"/>
+            {/*<Card title="Collected" value={totalPaidInvoices} type="collected"/>
             <Card title="Pending" value={totalPendingInvoices} type="pending"/>
             <Card title="Total Invoices" value={numberOfInvoices} type="invoices"/>
             <Card
                 title="Total Customers"
                 value={numberOfCustomers}
                 type="customers"
-            />
+            />*/}
         </>
     );
 }
