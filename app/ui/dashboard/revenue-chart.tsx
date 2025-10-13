ea5667771cca2a38915bdc4e5f095f92cfe50e71
mateico <mateo.rial@gmail.com>
2025-10-12 22:32:22 -0300

    commenting queries

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -16,11 +16,11 @@ export default async function RevenueChart() {
     const chartHeight = 350;
     // NOTE: Uncomment this code in Chapter 7
 
-    const {yAxisLabels, topLabel} = generateYAxis(revenue);
+   /* const {yAxisLabels, topLabel} = generateYAxis(revenue);
 
     if (!revenue || revenue.length === 0) {
         return <p className="mt-4 text-gray-400">No data available.</p>;
-    }
+    }*/
 
     return (
         <div className="w-full md:col-span-4">
@@ -36,24 +36,24 @@ export default async function RevenueChart() {
                         className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
                         style={{height: `${chartHeight}px`}}
                     >
-                        {yAxisLabels.map((label) => (
+                       {/* {yAxisLabels.map((label) => (
                             <p key={label}>{label}</p>
-                        ))}
+                        ))}*/}
                     </div>
 
-                    {revenue.map((month) => (
-                        <div key={month.month} className="flex flex-col items-center gap-2">
-                            <div
-                                className="w-full rounded-md bg-primary-400  "
-                                style={{
-                                    height: `${(chartHeight / topLabel) * month.revenue}px`,
-                                }}
-                            ></div>
-                            <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
-                                {month.month}
-                            </p>
-                        </div>
-                    ))}
+                    {/*{revenue.map((month) => (*/}
+                    {/*    <div key={month.month} className="flex flex-col items-center gap-2">*/}
+                    {/*        <div*/}
+                    {/*            className="w-full rounded-md bg-primary-400  "*/}
+                    {/*            style={{*/}
+                    {/*                height: `${(chartHeight / topLabel) * month.revenue}px`,*/}
+                    {/*            }}*/}
+                    {/*        ></div>*/}
+                    {/*        <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">*/}
+                    {/*            {month.month}*/}
+                    {/*        </p>*/}
+                    {/*    </div>*/}
+                    {/*))}*/}
                 </div>
                 <div className="flex items-center pb-2 pt-6">
                     <CalendarIcon className="h-5 w-5 text-gray-500"/>
