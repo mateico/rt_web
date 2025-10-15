f81e778d1b18f94a1dcf81a6ef7bf85f5db2751f
mateico <mateo.rial@gmail.com>
2025-10-15 15:57:24 -0300

    cotizaconi padding fixed

--- app/cotizacion/page.tsx
+++ app/cotizacion/page.tsx
@@ -1,15 +1,11 @@
-import Form from '@/app/ui/invoices/create-form';
-import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
 import SolicitarCotizacionForm from '@/app/ui/cotizacion/cotizacion-form';
 
 
 export default async function Page() {
-    const customers = await fetchCustomers();
 
     return (
-
-    <main className="max-w-3xl mx-auto py-10 px-4">
+    <main className="max-w-3xl mx-auto p-4">
         <SolicitarCotizacionForm />
     </main>
     );
