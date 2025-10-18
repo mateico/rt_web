39cbb42fa1a130d899e39d044ef226138a5b4cd2
mateico <mateo.rial@gmail.com>
2025-10-18 20:28:10 -0300

    footer centered

--- app/ui/Foot.tsx
+++ app/ui/Foot.tsx
@@ -2,15 +2,17 @@ import RFLogo from '@/app/ui/rf-logo';
 
 export default function Foot() {
     return (
-        <footer className="flex flex-col md:flex-row items-center justify-between bg-secondary-30 px-6 py-4 text-sm text-gray-600 shadow-inner hidden md:block">
-            <div className="mb-2 md:mb-0 flex items-center gap-4">
-                <RFLogo className="h-8 w-auto" />
-                <span>© 2025 Rufina Travel. All rights reserved.</span>
-            </div>
-            <div className="flex gap-4">
-                <a href="#">Privacy</a>
-                <a href="#">Terms</a>
-                <a href="#">Contact</a>
+        <footer className="bg-secondary-30 shadow-inner text-sm text-gray-600 hidden md:block">
+            <div className="mx-auto max-w-screen-xl flex-col md:flex-row items-center px-6 py-4">
+                <div className="mb-2 md:mb-0 flex items-center justify-between">
+                    <RFLogo className="h-8 w-auto"/>
+                    <span>© 2025 Rufina Travel. All rights reserved.</span>
+                </div>
+                <div className="flex gap-4">
+                    <a href="#">Privacy</a>
+                    <a href="#">Terms</a>
+                    <a href="#">Contact</a>
+                </div>
             </div>
         </footer>
     );
